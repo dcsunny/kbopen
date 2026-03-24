d9a122b8d88c5e3e9b2adc53311a213b1b1672ad
dcsunny <934831065@qq.com>
2026-03-24 17:43:48 +0800

    refactor(callback): 调整消息类型常量值
    - 将消息类型常量值从200-211范围调整为2001-2013范围
    - 添加新的直播间消息类型常量TypeMessageLiveBroadcastRoom
    - 所有消息类型常量值统一增加1801的基础偏移量

--- htsy/callback/type.go
+++ htsy/callback/type.go
@@ -5,16 +5,17 @@ const (
 )
 
 const (
-	TypeMessageText          = 200 //文本
-	TypeMessageImage         = 201 //图片
-	TypeMessageVideo         = 202 //视频
-	TypeMessageAudio         = 203 //音频
-	TypeMessageFile          = 204 //文件
-	TypeMessageLink          = 205 //链接
-	TypeMessageSticker       = 206 //表情
-	TypeMessageMiniProgram   = 207 //小程序
-	TypeMessageFreeVideo     = 208 //视频号
-	TypeMessageBizCard       = 209 //名片
-	TypeMessageRedPack       = 210 //红包
-	TypeMessageMsgCollection = 211 //聊天合集
+	TypeMessageText              = 2001 //文本
+	TypeMessageImage             = 2002 //图片
+	TypeMessageVideo             = 2003 //视频
+	TypeMessageAudio             = 2004 //音频
+	TypeMessageFile              = 2005 //文件
+	TypeMessageLink              = 2006 //链接
+	TypeMessageSticker           = 2007 //表情
+	TypeMessageMiniProgram       = 2008 //小程序
+	TypeMessageFreeVideo         = 2009 //视频号
+	TypeMessageBizCard           = 2010 //名片
+	TypeMessageRedPack           = 2011 //红包
+	TypeMessageMsgCollection     = 2012 //聊天合集
+	TypeMessageLiveBroadcastRoom = 2013 //直播间
 )
